e77922b5b6b2c803796f11da6e9ebbc097a37344
yuuuba <nsabu45@gmail.com>
2023-05-13 12:20:08 +0900

    postsコントローラメソッド関連

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,6 @@
 Rails.application.routes.draw do
-  get 'questions/new'
-  get 'questions/create'
-  get 'questions/destroy'
-  get 'answers/new'
-  get 'answers/create'
-  get 'answers/destroy'
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/edit'
-  get 'posts/update'
-  get 'posts/destroy'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'posts#index'
+  resources :posts
 end
